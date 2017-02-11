73d6eec7b711c25431bec63635e4258ebf305d20
xxyypp <yangpuypyp@outlook.com>
2017-02-11 12:26:26 +0000

    start

---
+++ db_2017_cw1_template.sql
@@ -0,0 +1,33 @@
+-- Q1 returns (name,dod)
+
+;
+
+-- Q2 returns (name)
+
+;
+
+-- Q3 returns (name)
+
+;
+
+-- Q4 returns (name,father,mother)
+
+;
+
+-- Q5 returns (name,popularity)
+
+;
+
+-- Q6 returns (name,forties,fifties,sixties)
+
+;
+
+
+-- Q7 returns (father,mother,child,born)
+
+;
+
+-- Q8 returns (father,mother,male)
+
+;
+
