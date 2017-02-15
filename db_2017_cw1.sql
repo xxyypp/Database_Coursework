14464e4bb2df9cd17ef6669191bb1f1b0c015614
xxyypp <yangpuypyp@outlook.com>
2017-02-15 16:44:12 +0000

    Q4 start

--- db_2017_cw1.sql
+++ db_2017_cw1.sql
@@ -1,7 +1,7 @@
 -- Q1 returns (name,dod)
 SELECT p_b.name,
 			 p_a.dod
-FROM 	 person p_a, person p_b
+FROM 	 person AS p_a, person AS p_b
 WHERE  p_a.name = p_b.mother
 AND		 p_a.dod IS NOT NULL
 ORDER BY p_b.name ASC
@@ -16,13 +16,34 @@ ORDER BY name ASC
 ;
 
 -- Q3 returns (name)
-SELECT p_a.name
-FROM person p_a, person p_b
-WHERE 
+SELECT DISTINCT p_mom.name
+FROM person AS p_chi,
+		 person AS p_mom
+WHERE (p_mom.name = p_chi.mother)
+AND		(p_chi.gender = 'M')
+INTERSECT
+SELECT DISTINCT p_mom.name
+FROM person AS p_chi,
+		 person AS p_mom
+WHERE (p_mom.name = p_chi.mother)
+AND		(p_chi.gender = 'F')
+ORDER BY name ASC
 ;
 
 -- Q4 returns (name,father,mother)
-
+SELECT DISTINCT p_first.name, p_first.dob, p_first.father, p_first.mother
+FROM person AS p_first,
+		 person AS p_other
+WHERE p_first.father IS NOT NULL
+AND p_first.mother IS NOT NULL
+AND p_first.father = p_other.father
+AND p_first.mother = p_other.mother
+--AND p_first.dob > p_other.dob
+--AND p_first.dob > ALL(SELECT p_other.dob
+--											FROM person AS p_other
+--											WHERE p_first.father = p_other.father
+--											AND p_first.mother = p_other.mother)
+ORDER BY p_first.father ASC
 ;
 
 -- Q5 returns (name,popularity)
